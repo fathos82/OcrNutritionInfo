acc3eef17d6521ff4238a21928c3d54718fa558f
Athos <fathos82@gmail.com>
2025-04-05 00:10:27 -0300

    Adding Pipeline ocr opt 6

--- client_main.py
+++ client_main.py
@@ -3,5 +3,5 @@ from structure.application import Application
 from structure.pipeline_factory import PipelineVariation
 from structure.runner_configuration import RunnerConfiguration
 from server.client_runner import ClientRunner
-config = RunnerConfiguration(pipeline_variation=PipelineVariation.PIPELINE_OCR_5, processing_name='Teste 01')
+config = RunnerConfiguration(pipeline_variation=PipelineVariation.PIPELINE_OCR_6, processing_name='Teste 02')
 Application(ClientRunner(config)).run()
--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from structure.pipeline_factory import PipelineVariation
 from structure.runner_configuration import RunnerConfiguration
 from server.server_application_runner import ServerApplicationRunner
 
-config = RunnerConfiguration(pipeline_variation=PipelineVariation.PIPELINE_1, processing_name='Teste 01')
+config = RunnerConfiguration(pipeline_variation=PipelineVariation.PIPELINE_OCR_6, processing_name='Teste 02')
 Application(ServerApplicationRunner(config)).run()
